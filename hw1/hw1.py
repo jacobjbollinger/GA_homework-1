d88c04d07afa9dbbfdc79724e7065b1d8a755b3c
William Holleran <whollera@gmail.com>
2013-11-08 03:33:29 +0000

    Updated HW1

--- hw1/hw1.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from sklearn import datasets
-from sklearn.neighbors import KNeighborsClassifier
-from __future__ import division
-
-iris = datasets.load_iris()
-iris_X = iris.data
-iris_y = iris.target
