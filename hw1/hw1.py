0cbfc1e08b4091b5c2d7fc570a110d1537a8257d
William Holleran <whollera@gmail.com>
2013-11-08 03:18:55 +0000

    Updated Homework #1

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,2 +1,8 @@
-import json
-print 'hi'
+import numpy as np
+from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from __future__ import division
+
+iris = datasets.load_iris()
+iris_X = iris.data
+iris_y = iris.target
